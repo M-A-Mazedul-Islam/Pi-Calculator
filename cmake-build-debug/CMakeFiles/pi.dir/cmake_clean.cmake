4d038d071eaaeb173711a170c454e493dc51e67b
Fake ID <junayeed@protonmail.com>
2017-03-17 14:35:17 +0600

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/pi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pi.dir/main.c.obj"
+  "pi.pdb"
+  "pi.exe"
+  "pi.exe.manifest"
+  "libpi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
